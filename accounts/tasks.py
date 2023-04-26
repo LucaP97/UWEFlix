07b4dd1b9a5ff75c730e2b78e698585bc6912e61
Luca <luca.pike7@gmail.com>
2023-04-27 00:27:07 +0100

    celery beat schedule updated

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -1,14 +1,21 @@
+import datetime
 import requests
 from celery import shared_task
 
+
+def is_last_day_of_month():
+    today = datetime.date.today()
+    last_day_of_month = (today.replace(day=1) + datetime.timedelta(days=32)).replace(day=1) - datetime.timedelta(days=1)
+    return today == last_day_of_month
+
 @shared_task
 def generate_statement():
-    url = 'http://127.0.0.1:8000/accounts/statements/'
-    # headers = {'Content-Type', 'application/json'}
+    if is_last_day_of_month():
+        url = 'http://127.0.0.1:8000/accounts/statements/'
 
-    response = requests.post(url)#, headers=headers)
+        response = requests.post(url)
 
-    if response.status_code == 201:
-        print('statement generated')
-    else:
-        print('statement not generated')
+        if response.status_code == 201:
+            print('statement generated')
+        else:
+            print('statement not generated')
