73a0396bb64879b7850170f85a60411f9b76a2b2
Luca <luca.pike7@gmail.com>
2023-05-01 19:37:09 +0100

    user viewset added

--- UWEFlix/views.py
+++ UWEFlix/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm
+from django.conf import settings
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from rest_framework.decorators import action
--- authenticate/views.py
+++ authenticate/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import *
+from .serializers import *
 
 # Create your views here.
+class UserViewSet(ModelViewSet):
+    # queryset = User.objects.all()
+    serializer_class = UserCreateSerializer
+
+    def get_queryset(self):
+        if hasattr(self.request.user, 'cinema_manager') or self.request.user.is_staff:
+            return User.objects.all()
+        elif self.request.user.is_authenticated:
+            return User.objects.filter(id=self.request.user.id)
+        else:
+            return User.objects.none()
+        
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return UserCreateSerializer
+        return UserSerializer
